64df86ddf9656d71a5b09c1e5fe3b6826aedf3d9
yellowsea <yunhai0644@163.com>
2022-12-18 22:36:34 +0800

    feat: 🎸 两数之和

--- src/07-two-number-sum.ts
+++ src/07-two-number-sum.ts
@@ -24,6 +24,43 @@ export function findTwoNumberSum1(arr: number[], n: number): number[] {
   return res;
 }
 
+/**
+ * @description 两数之和 - 双指针
+ * @param {number[]} arr
+ * @param {number} n
+ * @returns {number[]}
+ */
+
+// 时间复杂度 O(n)
+export function findTwoNumberSum2(arr: number[], n: number): number[] {
+  const res: number[] = [];
+  if (arr.length === 0) return res;
+
+  // 定义指针 i, j
+  let i = 0; // 头指针
+  let j = arr.length - 1; // 尾指针
+
+  // 循环
+  while (i < j) {
+    const n1 = arr[i];
+    const n2 = arr[j];
+    const sum = n1 + n2;
+    if (sum < n) {
+      // 小于目标值，头指针右移
+      i++;
+    } else if (sum > n) {
+      // 大于目标值，尾指针左移
+      j--;
+    } else {
+      // 等于
+      res.push(n1, n2);
+      break;
+    }
+  }
+  return res;
+}
+
 // const arr = [2, 7, 4, 10];
 // const n = 9;
-// console.log(findTwoNumberSum1(arr, n));
+// arr.sort((a, b) => a - b);
+// console.log(findTwoNumberSum2(arr, n));
--- tests/07-two-number-sum.test.ts
+++ tests/07-two-number-sum.test.ts
@@ -3,21 +3,30 @@
  * @author yellowsea
  */
 import { describe, expect, test } from 'vitest';
-import { findTwoNumberSum1 } from '../src/07-two-number-sum';
+import { findTwoNumberSum1, findTwoNumberSum2 } from '../src/07-two-number-sum';
 describe('07-two-number-sum', () => {
   test('正常情况', () => {
     const arr = [2, 7, 4, 10];
+    arr.sort((a, b) => a - b);
+
     const n = 9;
     expect(findTwoNumberSum1(arr, n)).toEqual([2, 7]);
+    expect(findTwoNumberSum2(arr, n)).toEqual([2, 7]);
   });
   test('空数组', () => {
     const arr = [];
     const n = 9;
+    arr.sort((a, b) => a - b);
+
     expect(findTwoNumberSum1(arr, n)).toEqual([]);
+    expect(findTwoNumberSum2(arr, n)).toEqual([]);
   });
   test('找不到结果', () => {
     const arr = [2, 7, 4, 10];
     const n = 102;
+    arr.sort((a, b) => a - b);
+
     expect(findTwoNumberSum1(arr, n)).toEqual([]);
+    expect(findTwoNumberSum2(arr, n)).toEqual([]);
   });
 });
